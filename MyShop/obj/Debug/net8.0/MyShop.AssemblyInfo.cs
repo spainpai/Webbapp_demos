e8c1e72186e03cba6f3a7bf801c2e64120cea902
spainpai <114737499+spainpai@users.noreply.github.com>
2024-11-10 21:13:47 +0100

    simplified table file and ran

--- MyShop/obj/Debug/net8.0/MyShop.AssemblyInfo.cs
+++ MyShop/obj/Debug/net8.0/MyShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44720d1551dec78d5f0062bb1dbbc87831de234d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0d70ad6c7f35d205fd72fe573b284a575599bfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
